c81425bda4c6a7999b3481f580823d195f6092ae
Vladimir Ivashkin <vlivashkin@yandex-team.ru>
2018-07-01 17:11:25 +0300

    add setup.py and travis config

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='py_graphs',
+    version='1.0',
+    packages=['py_graphs'],
+    url='',
+    license='MIT',
+    author='Vladimir Ivashkin',
+    author_email='vladimir.ivashkin@phystech.edu',
+    description='',
+    install_requires=[
+        'joblib',
+        'matplotlib',
+        'networkx',
+        'numpy',
+        'scikit-learn',
+        'scipy',
+        'tqdm'
+    ]
+)
