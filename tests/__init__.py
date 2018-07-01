43dc4fd67da466029acdec5f21326b76b7e07674
Vladimir Ivashkin <vlivashkin@yandex-team.ru>
2018-07-01 17:34:46 +0300

    fix unittest discover

---
+++ tests/__init__.py
@@ -0,0 +1,10 @@
+from .measure_common_tests import *
+from .GPDistance_tests import *
+from .spct_tests import *
+from .workflow_tests import *
+
+from .article_comparison.chebotarev_tests import *
+from .article_comparison.kivimaki_tests import *
+from .article_comparison.sommer_tests import *
+from .article_comparison.avrachenkov_tests import *
+from .article_comparison.yen_tests import *
