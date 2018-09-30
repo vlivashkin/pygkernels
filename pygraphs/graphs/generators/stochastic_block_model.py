5faf5e03bbff94c1021d8fcf700a5b57fde241f2
Vladimir Ivashkin <illusionww@gmail.com>
2018-09-30 13:16:03 +0300

    remove old generator

--- pygraphs/graphs/generators/stochastic_block_model.py
+++ pygraphs/graphs/generators/stochastic_block_model.py
@@ -1,58 +1,34 @@
 import logging
-from itertools import product
 
 import numpy as np
-from sklearn.utils import deprecated
 
 
-@deprecated()
 class StochasticBlockModel:
-    def __init__(self, n_nodes, n_classes, p_in, p_out, cluster_sizes=None, probabilities=None):
+    def __init__(self, n_nodes, n_classes, p_in, p_out):
         self.n = n_nodes
         self.k = n_classes
         self.p_in = p_in
         self.p_out = p_out
 
-        if cluster_sizes is not None:
-            if len(cluster_sizes) == n_classes and np.sum(cluster_sizes) == n_nodes:
-                self.cluster_sizes = cluster_sizes
-            else:
-                raise ValueError()
-        else:
-            cluster_size = self.n // self.k
-            self.cluster_sizes = [cluster_size] * self.k
-            self.n = cluster_size * self.k
-
-        if probabilities is not None:
-            self.probabilities = probabilities
-        else:
-            self.probabilities = np.zeros((self.k, self.k))
-            for i, j in product(range(self.k), range(self.k)):
-                self.probabilities[i][j] = self.p_in if i == j else self.p_out
-
     def generate_graph(self):
         nodes = []
         for i in range(self.k):
-            nodes.extend([i] * self.cluster_sizes[i])
+            nodes.extend([i] * (self.n // self.k))
 
         edges = np.zeros((self.n, self.n))
-
-        random_matrix = [[[] for _ in range(self.k)] for _ in range(self.k)]
-        for i in range(self.k):
-            for j in range(i, self.k):
-                a = np.random.choice([0, 1], edges.shape, p=[1 - self.probabilities[i][j], self.probabilities[i][j]])
-                random_matrix[i][j] = a
-                random_matrix[j][i] = a
-
-        for i in range(edges.shape[0]):
-            for j in range(edges.shape[1]):
-                edges[i][j] = random_matrix[nodes[i]][nodes[j]][i][j]
+        for i in range(self.n):
+            for j in range(i + 1, self.n):
+                p = self.p_in if nodes[i] == nodes[j] else self.p_out
+                k = np.random.choice([0, 1], p=[1 - p, p])
+                if k:
+                    edges[i][j] = 1
+                    edges[j][i] = 1
 
         return edges, nodes
 
     def generate_graphs(self, n_graphs):
-        logging.info('StochasticBlockModel: count={}, n={}, k={}, p_in={}, p_out={}, cluster_sizes={}'.format(
-            n_graphs, self.n, self.k, self.p_in, self.p_out, self.cluster_sizes))
+        logging.info('StochasticBlockModel2: count={}, n={}, k={}, p_in={}, p_out={}'.format(
+            n_graphs, self.n, self.k, self.p_in, self.p_out))
 
         info = {
             'name': 'count:{}, n:{}, k:{}, p_in:{}, p_out:{}'.format(n_graphs, self.n, self.k, self.p_in, self.p_out),
