7557eafd1ed2241d86c627233eeb97e801daada7
Vladimir Ivashkin <illusionww@gmail.com>
2018-10-06 23:18:26 +0300

    optimize imports

--- pygraphs/scenario.py
+++ pygraphs/scenario.py
@@ -8,8 +8,38 @@ from joblib import Parallel, delayed
 from sklearn.metrics import adjusted_rand_score
 from tqdm import tqdm_notebook as tqdm
 
-from pygraphs.colors import d3, d3_category20
 from pygraphs.util import ddict2dict
+
+d3_category20 = [
+    '#1f77b4',
+    '#aec7e8',
+    '#ff7f0e',
+    '#ffbb78',
+    '#2ca02c',
+    '#98df8a',
+    '#d62728',
+    '#ff9896',
+    '#9467bd',
+    '#c5b0d5',
+    '#8c564b',
+    '#c49c94',
+    '#e377c2',
+    '#f7b6d2',
+    '#7f7f7f',
+    '#c7c7c7',
+    '#bcbd22',
+    '#dbdb8d',
+    '#17becf',
+    '#9edae5'
+]
+
+
+def d3():
+    idx = 0
+    while True:
+        yield d3_category20[idx], d3_category20[idx + 1]
+        idx = (idx + 2) % 20
+
 
 d3_right_order = []
 for i in range(6):
