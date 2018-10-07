20c6d7ddbb0d1fadc000efceef2f39ad2d0478d5
Vladimir Ivashkin <illusionww@gmail.com>
2018-10-07 18:13:01 +0300

    improve graphs generation info

--- pygraphs/graphs/generator/stochastic_block_model.py
+++ pygraphs/graphs/generator/stochastic_block_model.py
@@ -8,20 +8,33 @@ class StochasticBlockModel:
         self.n = n_nodes
         self.k = n_classes
 
-        if p_in is not None and p_out is not None:
+        if p_in is not None and p_out is not None and probability_matrix is None:
             self.p_in = p_in
             self.p_out = p_out
-        elif probability_matrix is not None:
+            self.probability_matrix_mode = False
+        elif p_in is None and p_out is None and probability_matrix is not None:
             assert probability_matrix.shape[0] == n_classes and probability_matrix.shape[1] == n_classes
             self.probability_matrix = probability_matrix
+            self.probability_matrix_mode = True
         else:
             raise ValueError('provide either (p_in, p_out) or probability_matrix')
 
         if cluster_sizes is not None:
             assert len(cluster_sizes) == n_classes
             self.cluster_sizes = cluster_sizes
+            self.cluster_sizes_mode = False
         else:
             self.cluster_sizes = [self.n // self.k] * self.k
+            self.cluster_sizes_mode = True
+
+        name_parts = ['n={}'.format(self.n), 'k={}'.format(self.k)]
+        if self.cluster_sizes_mode:
+            name_parts.append('cluster_sizes_mode')
+        if self.probability_matrix_mode:
+            name_parts.append('probability_matrix_mode')
+        else:
+            name_parts.append('p_in={}, p_out=()'.format(self.p_in, self.p_out))
+        self.name = ', '.join(name_parts)
 
     def generate_graph(self):
         nodes = []
@@ -43,17 +56,21 @@ class StochasticBlockModel:
         return edges, nodes
 
     def generate_graphs(self, n_graphs):
-        logging.info('StochasticBlockModel: count={}, n={}, k={}, p_in={}, p_out={}'.format(
-            n_graphs, self.n, self.k, self.p_in, self.p_out))
+        logging.info('StochasticBlockModel: count={}, {}'.format(n_graphs, self.name))
 
         info = {
-            'name': 'count:{}, n:{}, k:{}, p_in:{}, p_out:{}'.format(n_graphs, self.n, self.k, self.p_in, self.p_out),
+            'name': 'count:{}, {}'.format(n_graphs, self.name),
             'n_graphs': n_graphs,
             'n': self.n,
             'k': self.k,
-            'p_in': self.p_in,
-            'p_out': self.p_out
+            'cluster_sizes_mode': self.cluster_sizes_mode,
+            'probability_matrix_mode': self.probability_matrix_mode
         }
+        if self.probability_matrix_mode:
+            info.update({
+                'p_in': self.p_in,
+                'p_out': self.p_out
+            })
         graphs = [self.generate_graph() for _ in range(n_graphs)]
         return graphs, info
 
