ed2843b06418232c8a8d99cad47907d0f2107d6c
Vladimir Ivashkin <illusionww@gmail.com>
2018-10-07 17:46:26 +0300

    add probability_matrix to graph generator

--- pygraphs/graphs/generator/stochastic_block_model.py
+++ pygraphs/graphs/generator/stochastic_block_model.py
@@ -4,13 +4,24 @@ import numpy as np
 
 
 class StochasticBlockModel:
-    def __init__(self, n_nodes, n_classes, p_in, p_out, cluster_sizes=None):
+    def __init__(self, n_nodes, n_classes, cluster_sizes=None, p_in=None, p_out=None, probability_matrix=None):
         self.n = n_nodes
         self.k = n_classes
-        self.p_in = p_in
-        self.p_out = p_out
 
-        self.cluster_sizes = cluster_sizes if cluster_sizes is not None else [self.n // self.k] * self.k
+        if p_in is not None and p_out is not None:
+            self.p_in = p_in
+            self.p_out = p_out
+        elif probability_matrix is not None:
+            assert probability_matrix.shape[0] == n_classes and probability_matrix.shape[1] == n_classes
+            self.probability_matrix = probability_matrix
+        else:
+            raise ValueError('provide either (p_in, p_out) or probability_matrix')
+
+        if cluster_sizes is not None:
+            assert len(cluster_sizes) == n_classes
+            self.cluster_sizes = cluster_sizes
+        else:
+            self.cluster_sizes = [self.n // self.k] * self.k
 
     def generate_graph(self):
         nodes = []
@@ -20,7 +31,10 @@ class StochasticBlockModel:
         edges = np.zeros((self.n, self.n))
         for i in range(self.n):
             for j in range(i + 1, self.n):
-                p = self.p_in if nodes[i] == nodes[j] else self.p_out
+                if self.probability_matrix is None:
+                    p = self.p_in if nodes[i] == nodes[j] else self.p_out
+                else:
+                    p = self.probability_matrix[i][j]
                 k = np.random.choice([0, 1], p=[1 - p, p])
                 if k:
                     edges[i][j] = 1
