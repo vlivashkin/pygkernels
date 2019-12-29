0a14db333d24ec3072574b0813aa614fac2814d8
illusionww <Ga12m10p7>
2019-12-29 14:15:40 +0300

    fixes for torch version

--- pygraphs/cluster/_kmeans_numpy.py
+++ pygraphs/cluster/_kmeans_numpy.py
@@ -1,55 +1,40 @@
 import numpy as np
-from numba import jit
 
 
-@jit(nopython=True, cache=True)
 def _hKh(hk: np.array, ei: np.array, K: np.array):
-    hk_ei = np.expand_dims((hk - ei), axis=0)
-    return hk_ei.dot(K).dot(hk_ei.T)[0, 0]
+    hk_ei = hk - ei
+    return np.einsum('i,ij,j->', hk_ei, K, hk_ei)
 
 
-@jit(nopython=True, cache=True)
-def _inertia(h: np.array, K: np.array, labels: np.array):
-    n = K.shape[0]
-    e = np.eye(n, dtype=np.float64)
-    return np.array([_hKh(h[labels[i]], e[i], K) for i in range(0, n)]).sum()
+def _inertia(h: np.array, e, K: np.array, labels: np.array):
+    h_e = h[labels] - e
+    return np.einsum('ij,jk,ki->', h_e, K, h_e)
 
 
-@jit(nopython=True, cache=True)
 def _vanilla_predict(K: np.array, h: np.array, max_iter: int):
     n_clusters, n = h.shape
     e = np.eye(n, dtype=np.float64)
 
     labels, success = np.zeros((n,), dtype=np.uint8), True
-    for _ in range(max_iter):
-        # fix h, update U
-        U, l = np.zeros((n, n_clusters), dtype=np.float64), np.zeros((n,), dtype=np.uint8)
-        for i in range(n):
-            k_star, min_inertia = -1, np.inf
-            for k in range(n_clusters):
-                inertia = _hKh(h[k], e[i], K)
-                if inertia < min_inertia:
-                    k_star, min_inertia = k, inertia
-            U[i, k_star] = 1
-            l[i] = k_star
-
-        # early stop
-        if np.all(labels == l):  # nothing changed
+    for iter in range(max_iter):
+        h_e = h[:, None] - e[None]  # [k, n, n]
+        l = np.argmin(np.einsum('kni,ij,knj->kn', h_e, K, h_e), axis=0)
+        if np.all(labels == l):  # early stop
             break
         labels = l
 
-        # fix U, update h
-        nn = np.expand_dims(np.sum(U, axis=0), axis=0)
+        U = np.zeros((n, n_clusters), dtype=np.float64)
+        U[range(n), labels] = 1
+        nn = U.sum(axis=0, keepdims=True)
         if np.any(nn == 0):  # empty cluster! exit with success=False
             success = False
             break
         h = (U / nn).T
 
-    inertia = _inertia(h, K, labels)
+    inertia = _inertia(h, e, K, labels)
     return labels, inertia, success
 
 
-@jit(nopython=True, cache=True)
 def _iterative_predict(K: np.array, h: np.array, U: np.array, l: np.array, nn: np.array, max_iter: int, eps: float):
     n_clusters, n = h.shape
     e = np.eye(n, dtype=np.float64)
@@ -59,23 +44,19 @@ def _iterative_predict(K: np.array, h: np.array, U: np.array, l: np.array, nn: n
         node_order = np.array(list(range(n)))
         np.random.shuffle(node_order)
         for i in node_order:  # for each node
-            k_star, minΔJ = -1, np.inf
-            for k in range(n_clusters):
-                ΔJ1 = nn[k] / (nn[k] + 1 + eps) * _hKh(h[k], e[i], K)
-                ΔJ2 = nn[l[i]] / (nn[l[i]] - 1 + eps) * _hKh(h[l[i]], e[i], K)
-                ΔJ = ΔJ1 - ΔJ2
-                if ΔJ < minΔJ:
-                    minΔJ, k_star = ΔJ, k
+            h_ei = h - e[i][None]
+            ΔJ1 = nn / (nn + 1 + eps) * np.einsum('ki,ij,kj->k', h_ei, K, h_ei)
+            k_star = np.argmin(ΔJ1)
+            ΔJ2 = nn[l[i]] / (nn[l[i]] - 1 + eps) * _hKh(h[l[i]], e[i], K)
+            minΔJ = ΔJ1[k_star] - ΔJ2
             if minΔJ < 0 and l[i] != k_star:
                 if nn[l[i]] == 1:  # it will cause empty cluster! exit with success=False
-                    inertia = _inertia(h, K, labels)
+                    inertia = _inertia(h, e, K, labels)
                     return labels, inertia, False
                 h[l[i]] = 1. / (nn[l[i]] - 1 + eps) * (nn[l[i]] * h[l[i]] - e[i])
-                U[i, l[i]] = 0
-                nn[l[i]] -= 1
                 h[k_star] = 1. / (nn[k_star] + 1 + eps) * (nn[k_star] * h[k_star] + e[i])
-                U[i, k_star] = 1
-                nn[k_star] += 1
+                U[i, l[i]], U[i, k_star] = 0, 1
+                nn[l[i]], nn[k_star] = nn[l[i]] - 1, nn[k_star] + 1
                 l[i] = k_star
 
         # early stop
@@ -83,5 +64,5 @@ def _iterative_predict(K: np.array, h: np.array, U: np.array, l: np.array, nn: n
             break
         labels = l.copy()
 
-    inertia = _inertia(h, K, labels)
+    inertia = _inertia(h, e, K, labels)
     return labels, inertia, ~np.isnan(inertia)
--- pygraphs/cluster/_kmeans_pytorch.py
+++ pygraphs/cluster/_kmeans_pytorch.py
@@ -6,7 +6,7 @@ def torch_func(func):
     def wrapper(*args, device=1, **kwargs):
         with torch.no_grad():
             args = [torch.from_numpy(x).float().to(device)
-                    if type(x) == np.ndarray and x.dtype in [np.float32, np.float64] else x
+                    if type(x) in [np.ndarray, np.memmap] and x.dtype in [np.float32, np.float64] else x
                     for x in args]
             results = func(*args, **kwargs)
             results = [x.numpy() if type(x) == torch.tensor else x for x in results]
@@ -16,14 +16,12 @@ def torch_func(func):
 
 
 def _hKh(hk, ei, K):
-    hk_ei = (hk - ei).unsqueeze(dim=0)
-    return hk_ei.mm(K).mm(hk_ei.transpose(0, 1))[0, 0]
-
+    hk_ei = hk - ei
+    return torch.einsum('i,ij,j->', [hk_ei, K, hk_ei])
 
 def _inertia(h, e, K, labels):
-    n = K.shape[0]
-    return np.array([_hKh(h[labels[i]], e[i], K) for i in range(0, n)]).sum()
-
+    h_e = h[labels] - e
+    return torch.einsum('ij,jk,ki->', [h_e, K, h_e])
 
 @torch_func
 def _vanilla_predict(K, h, max_iter: int, device=1):
@@ -31,26 +29,16 @@ def _vanilla_predict(K, h, max_iter: int, device=1):
     e = torch.eye(n, dtype=torch.float32).to(device)
 
     labels, success = np.zeros((n,), dtype=np.uint8), True
-    for _ in range(max_iter):
-        # fix h, update U
-        U = torch.zeros((n, n_clusters), dtype=torch.float32).to(device)
-        l = np.zeros((n,), dtype=np.uint8)
-        for i in range(n):
-            k_star, min_inertia = -1, np.inf
-            for k in range(n_clusters):
-                inertia = _hKh(h[k], e[i], K)
-                if inertia < min_inertia:
-                    k_star, min_inertia = k, inertia
-            U[i, k_star] = 1
-            l[i] = k_star
-
-        # early stop
-        if np.all(labels == l):  # nothing changed
+    for iter in range(max_iter):
+        h_e = h.unsqueeze(1) - e.unsqueeze(0)  # [k, n, n]
+        l = torch.argmin(torch.einsum('kni,ij,knj->kn', [h_e, K, h_e]), dim=0)
+        if torch.all(labels == l):  # early stop
             break
         labels = l
 
-        # fix U, update h
-        nn = torch.unsqueeze(torch.sum(U, dim=0), dim=0)
+        U = torch.zeros((n, n_clusters), dtype=torch.float32).to(device)
+        U[range(n), labels] = 1
+        nn = U.sum(dim=0, keepdim=True)
         if torch.any(nn == 0):  # empty cluster! exit with success=False
             success = False
             break
@@ -70,27 +58,22 @@ def _iterative_predict(K, h, U, l, nn, max_iter: int, eps: float, device=1):
         node_order = np.array(list(range(n)))
         np.random.shuffle(node_order)
         for i in node_order:  # for each node
-            k_star, minΔJ = -1, np.inf
-            for k in range(n_clusters):
-                ΔJ1 = nn[k] / (nn[k] + 1 + eps) * _hKh(h[k], e[i], K)
-                ΔJ2 = nn[l[i]] / (nn[l[i]] - 1 + eps) * _hKh(h[l[i]], e[i], K)
-                ΔJ = ΔJ1 - ΔJ2
-                if ΔJ < minΔJ:
-                    minΔJ, k_star = ΔJ, k
+            h_ei = h - e[i][None]
+            ΔJ1 = nn / (nn + 1 + eps) * torch.einsum('ki,ij,kj->k', [h_ei, K, h_ei])
+            k_star = torch.argmin(ΔJ1)
+            ΔJ2 = nn[l[i]] / (nn[l[i]] - 1 + eps) * _hKh(h[l[i]], e[i], K)
+            minΔJ = ΔJ1[k_star] - ΔJ2
             if minΔJ < 0 and l[i] != k_star:
                 if nn[l[i]] == 1:  # it will cause empty cluster! exit with success=False
                     inertia = _inertia(h, e, K, labels)
                     return labels, inertia, False
                 h[l[i]] = 1. / (nn[l[i]] - 1 + eps) * (nn[l[i]] * h[l[i]] - e[i])
-                U[i, l[i]] = 0
-                nn[l[i]] -= 1
                 h[k_star] = 1. / (nn[k_star] + 1 + eps) * (nn[k_star] * h[k_star] + e[i])
-                U[i, k_star] = 1
-                nn[k_star] += 1
+                U[i, l[i]], U[i, k_star] = 0, 1
+                nn[l[i]], nn[k_star] = nn[l[i]] - 1, nn[k_star] + 1
                 l[i] = k_star
 
-        # early stop
-        if np.all(labels == l):  # nothing changed
+        if np.all(labels == l):  # early stop
             break
         labels = l.copy()
 
